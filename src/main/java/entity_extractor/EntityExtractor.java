7f3ca54e1727b21de66d003148e5c6d45e1850cf
Leonidas Tsekouras <leotsekouras@gmail.com>
2016-11-11 14:19:48 +0200

    Remove redundant "public" keyword from interface

--- src/main/java/entity_extractor/EntityExtractor.java
+++ src/main/java/entity_extractor/EntityExtractor.java
@@ -3,9 +3,9 @@ package entity_extractor;
 import java.io.File;
 
 public interface EntityExtractor {
-    public void setApiKey(String apiKey);
+    void setApiKey(String apiKey);
 
-    public void setOutputDir(File dir);
+    void setOutputDir(File dir);
 
-    public TextEntities getEntities(File input);
+    TextEntities getEntities(File input);
 }
