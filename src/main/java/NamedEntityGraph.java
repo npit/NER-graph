14efc2022c55a3a2642c0e7ddb3de6ec801e2e6f
Leonidas Tsekouras <leotsekouras@gmail.com>
2016-11-18 14:50:08 +0200

    Add comparison of 2 texts with normal n-gram and NEG graphs

--- src/main/java/NamedEntityGraph.java
+++ src/main/java/NamedEntityGraph.java
@@ -1,7 +1,10 @@
 import entity_extractor.EntityExtractor;
 import entity_extractor.TextEntities;
 import entity_extractor.open_calais_extractor.OpenCalaisExtractor;
+import gr.demokritos.iit.jinsect.documentModel.comparators.NGramCachedGraphComparator;
+import gr.demokritos.iit.jinsect.documentModel.representations.DocumentNGramGraph;
 import gr.demokritos.iit.jinsect.documentModel.representations.DocumentWordGraph;
+import gr.demokritos.iit.jinsect.structs.GraphSimilarity;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -13,7 +16,8 @@ public class NamedEntityGraph {
 
         File input = new File(inputFolder);
         EntityExtractor entityExtractor = new OpenCalaisExtractor();
-        ArrayList<DocumentWordGraph> graphs = new ArrayList<>();
+        ArrayList<DocumentWordGraph> entityGraphs = new ArrayList<>();
+        ArrayList<DocumentNGramGraph> nGramGraphs = new ArrayList<>();
 
         try {
             if (input.isDirectory()) {
@@ -28,11 +32,17 @@ public class NamedEntityGraph {
                         String entityText = entities.getEntityText();
                         System.out.println(entityText);
 
-                        // Create a word graph
+                        // Create entity word graph
                         DocumentWordGraph wordGraph = new DocumentWordGraph();
                         wordGraph.setDataString(entityText);
 
-                        graphs.add(wordGraph);
+                        entityGraphs.add(wordGraph);
+
+                        // Create normal n-gram graph
+                        DocumentNGramGraph nGramGraph = new DocumentWordGraph();
+                        nGramGraph.setDataString(entities.getText());
+
+                        nGramGraphs.add(nGramGraph);
 
                         System.out.println("[main] Got " + entities.getEntities().size() + " extracted entities from " + file + "\n");
                     } else {
@@ -42,6 +52,34 @@ public class NamedEntityGraph {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+
+        // Create comparator object
+        NGramCachedGraphComparator comparator = new NGramCachedGraphComparator();
+
+        // Compare 2 first entity graphs
+        if (entityGraphs.size() >= 2) {
+            // Get graphs to compare
+            DocumentWordGraph entityGraph1 = entityGraphs.get(0);
+            DocumentWordGraph entityGraph2 = entityGraphs.get(1);
+
+            // Get similarity of graphs
+            GraphSimilarity entitySimilarity = comparator.getSimilarityBetween(entityGraph1, entityGraph2);
+
+            double entitySimVal = entitySimilarity.getOverallSimilarity();
+            System.out.println("Named Entity Graph similarity:\t" + entitySimVal);
+        }
+
+        // Compare 2 first n-gram graphs
+        if (nGramGraphs.size() >= 2) {
+            DocumentNGramGraph nGramGraph1 = nGramGraphs.get(0);
+            DocumentNGramGraph nGramGraph2 = nGramGraphs.get(1);
+
+            // Get similarity of n-gram graphs
+            GraphSimilarity nGramSimilarity = comparator.getSimilarityBetween(nGramGraph1, nGramGraph2);
+
+            double nGramSimVal = nGramSimilarity.getOverallSimilarity();
+            System.out.println("N-gram Graph similarity:\t\t" + nGramSimVal);
         }
     }
 }
