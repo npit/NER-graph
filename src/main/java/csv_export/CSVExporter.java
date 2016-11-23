d5c56e9440736a705ad27c9cbbb939a3bbef8106
Leonidas Tsekouras <leotsekouras@gmail.com>
2016-11-23 15:56:59 +0200

    Add header row to the exported CSV file

--- src/main/java/csv_export/CSVExporter.java
+++ src/main/java/csv_export/CSVExporter.java
@@ -1,6 +1,7 @@
 package csv_export;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
@@ -12,15 +13,47 @@ public class CSVExporter {
     @SuppressWarnings("SameParameterValue")
     public static void exportCSV(String filename, List<String> placeholders, List<ComparisonContainer> results) {
         Logger LOGGER = Logger.getLogger("NamedEntityGraph");
+        StringBuilder sb;
 
         try{
             PrintWriter writer = new PrintWriter(filename, "UTF-8");
             writer.println("sep=" + separator);
 
-            //todo: print headers
+            // Print header for text titles
+            writer.print("Text 1, Text 2,");
 
-            // Print results to file
-            StringBuilder sb;
+            List<String> comparisons = new ArrayList<>();
+
+            // Gather names of comparisons that were made
+            comparisons.add("n-gram graph");
+            comparisons.add("word graph");
+            for (String ph : placeholders) {
+                comparisons.add("PH (" + ph + ")");
+            }
+            for (String ph : placeholders) {
+                comparisons.add("PHSS (" + ph + ")");
+            }
+            comparisons.add("rand");
+
+            // Create headers with graph similarity measure
+            sb = new StringBuilder();
+
+            for (String s : comparisons) {
+                sb.append(s);
+                sb.append(" val");
+                sb.append(separator);
+                sb.append(s);
+                sb.append(" cont");
+                sb.append(separator);
+                sb.append(s);
+                sb.append(" size");
+                sb.append(separator);
+            }
+
+            sb.delete(sb.length() - separator.length(), sb.length());
+            writer.println(sb.toString());
+
+            // Print results
             for (ComparisonContainer cont : results) {
                 sb = new StringBuilder();
 
