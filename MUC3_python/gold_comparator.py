4ef4ae26df4c4ac78a8366560cf24fc80f99879e
Leonidas Tsekouras <leotsekouras@gmail.com>
2016-12-03 16:33:18 +0200

    Add basic python script structure

--- MUC3_python/gold_comparator.py
+++ MUC3_python/gold_comparator.py
@@ -1,4 +1,30 @@
-def main():
-    print("frist")
+import os
 
-main()
+
+def get_text_type(path, file):
+    print(path + file)
+    return "TODO"
+
+
+def get_text_categories(path):
+    # Dictionary to keep types for each text
+    text_types = {}
+
+    files = os.listdir(path)
+
+    for file in files:
+        text_type = get_text_type(path, file)
+        text_types[file] = text_type
+
+    return text_types
+
+
+def main():
+    path = "../texts/input/"
+
+    text_categories = get_text_categories(path)
+
+    # print(text_categories)
+
+if __name__ == '__main__':
+    main()
