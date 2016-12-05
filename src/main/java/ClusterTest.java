1335b7575b9725477f9b2234f1fb126a3b2d6b0e
Leonidas Tsekouras <leotsekouras@gmail.com>
2016-12-05 12:46:16 +0200

    Add class for clustering test

---
+++ src/main/java/ClusterTest.java
@@ -0,0 +1,11 @@
+public class ClusterTest {
+    public static void main(String[] args) {
+        ClusterTest ct = new ClusterTest();
+
+        ct.start();
+    }
+
+    private void start() {
+        System.out.println("clustering test!");
+    }
+}
