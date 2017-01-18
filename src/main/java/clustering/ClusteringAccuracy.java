759cae9327e200372ed12f346609c2089bf33bd1
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-01-18 13:43:32 +0200

    Add todos in the code for accuracy measures

--- src/main/java/clustering/ClusteringAccuracy.java
+++ src/main/java/clustering/ClusteringAccuracy.java
@@ -14,12 +14,27 @@ public class ClusteringAccuracy {
     }
 
     private void start() {
-        //todo
         DummyData data = new DummyData();
         Clusters g = data.getGroundTruthClusters();
         Clusters a = data.getAlgorithmClusters();
 
         System.out.println(g);
         System.out.println(a);
+
+        // todo: Find mapping (f) of ground truth clusters -> algorithm's clusters
+
+        // todo: Calculate Pr (weighted avg. of PRi's over all ground truth clusters)
+
+        // todo: Calculate Re (weighted avg. of Rei's over all ground truth clusters)
+
+        // todo: Calculate CPr (avg of CPRi's)
+
+        // todo: Calculate PCPr
+        //noinspection StatementWithEmptyBody
+        if (g.getClustersNum() > a.getClustersNum()) {
+            // k/k' * CPr
+        } else {
+            // k'/k * CPr
+        }
     }
 }
