d6f9de1ab5120d3be1517523f2feb35847c3c8ac
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-01-23 21:48:27 +0200

    Implement reading ELKI's output clusters

--- src/main/java/clustering/accuracy_measures/data/ElkiOpticsXiData.java
+++ src/main/java/clustering/accuracy_measures/data/ElkiOpticsXiData.java
@@ -18,7 +18,18 @@ public class ElkiOpticsXiData implements ClustersData {
         algClusters = new Clusters();
         groundTruth = new Clusters();
 
-        //todo: Get algorithm clusters from OPTICSXi ELKI output folder
+        // Get algorithm clusters from OPTICSXi ELKI output folder
+        File[] files = new File(elkiClustersPath).listFiles();
+        if (files != null) {
+            for (File file : files) {
+
+                // Ignore settings.txt and optics-clusterorder.txt files
+                if (file.isFile() && !file.getName().equals("settings.txt") && !file.getName().equals("optics-clusterorder.txt")) {
+                    // Get cluster from the file and add it to the algorithm clusters
+                    algClusters.addCluster(getClusterFromFile(file));
+                }
+            }
+        }
 
         // Get ground truth clusters from python-exported ground truth text file
         try {
@@ -51,6 +62,41 @@ public class ElkiOpticsXiData implements ClustersData {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Get a file that is an OPTICSXi cluster from ELKI, and create a SimpleCluster from it.
+     * Sets the name (if found in the file, which it should) and adds the cluster IDs.
+     * @param f File to read
+     * @return  Cluster made from ELKI output file
+     */
+    private SimpleCluster getClusterFromFile(File f) {
+        SimpleCluster c = new SimpleCluster();
+
+        try {
+            // Open the file
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(f), "UTF-8"));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Check the line's prefix to process it appropriately
+                if (line.startsWith("# Cluster name: ")) {
+                    // Get cluster name
+                    String name = line.split(" ")[3];
+
+                    c.setName(name);
+                } else if (line.startsWith("ID=")) {
+                    String idStr = line.split(" ")[0].split("=")[1];
+
+                    c.addText(Integer.parseInt(idStr));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return c;
     }
 
     @Override
